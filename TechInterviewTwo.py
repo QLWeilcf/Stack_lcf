d0ce8b366838812e371d5666b94e299272087307
QLWeilcf <1226824818@qq.com>
2018-04-12 13:11:39 +0800

    Update TechInterviewTwo.py

--- TechInterviewTwo.py
+++ TechInterviewTwo.py
@@ -68,4 +68,18 @@ def maxtNum(num):
     
 #进阶：根据字典序算法输出一个数的全排列
 
+# offer 45
+#要求：0到n-1排成一圈，从0开始每次数m个数删除，求最后剩余的数
+#可以用模拟的方式求，模拟取数过程，效率低些
+#思路：当 n > 1 时： f(n,m) = [f(n-1, m)+m]%n,当 n = 1 时： f(n,m)=0，关键是推导出关系表达式
+
+def last_num(n, m):
+    ret = 0
+    if n == 1:
+        return 0
+    for i in range(2, n+1):
+        ret = (m + ret) % i
+    return ret
+
+
 
